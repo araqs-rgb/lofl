9a264a1b77e1cc1dce873c5c7addb770c5bd3bfe
Akarys <beiseculov@gmail.com>
2023-05-02 00:01:41 +0600

    hell yeah2

--- app/src/main/java/com/example/lofl/fragments/FinalMessageFragment.kt
+++ app/src/main/java/com/example/lofl/fragments/FinalMessageFragment.kt
@@ -1,11 +1,14 @@
 package com.example.lofl.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.lofl.R
+import com.example.lofl.RegisterActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
---
+++ app/src/main/java/com/example/lofl/inputChat.kt
@@ -0,0 +1,11 @@
+package com.example.lofl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inputChat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_chat)
+    }
+}
